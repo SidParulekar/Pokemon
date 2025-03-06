145889fb24b2015944420987230fa27e855b762a
SidParulekar <76064146+SidParulekar@users.noreply.github.com>
2025-03-06 20:31:26 +0530

    Created Classes and Functions for Player, Pokemon and Professor Oak
    Created Player Class and declared variable for player name and instance of player pokemon. Created functions for Player to enter name and choose pokemon.
    
    Created PokemonType enum class for different pokemon types.
    
    Created the Pokemon class and declared variables for pokemon name, type and health. Defined parameterized constructor for initializing the variables for each Pokemon instance and the Attack function.
    
    Created ProfessorOak class. Defined different functions for different dialogues.
    
    Created objects of Player and ProfessorOak classes in the main function and called all the necessary functions of the two classes.

--- Pokemon/main.cpp
+++ Pokemon/main.cpp
@@ -2,83 +2,196 @@
 
 using namespace std;
 
-enum class Pokemon
+enum class PokemonOptions
 {
-    Bulbasaur, 
+    Bulbasaur = 1, 
     Charmander,
     Squirtle,
     InvalidChoice
 };
 
-string ChosenPokemonName(Pokemon choice)
+enum class PokemonType
 {
-    switch (choice)
+    FIRE,
+    GRASS,
+    WATER,
+    ELECTRIC
+};
+
+class Pokemon
+{
+public:
+    string name;
+    PokemonType type;
+    int health;
+
+    Pokemon()
     {
-    case Pokemon::Bulbasaur:
-        return "Bulbasur";
-        break;
-
-    case Pokemon::Charmander:
-        return "Charmander";
-        break;
-
-    case Pokemon::Squirtle:
-        return "Squirtle";
-        break;
-
     }
-}
 
-int main() {
-    
-    cout << "Trainer! Before you embark on your journey, the Pokemon world needs to know who you are." << endl;
-    cout << "Time to step up and tell us your name and remember a great name is the start of every great adventure." << endl;
-
-    string player_name; 
-    cin >> player_name;
-
-    cout << "\nWelcome " + player_name + " to the start of your adventure in the Pokemon World." << endl;
-
-    cout << "\nI am Professor Oak. Today is a momentous day—you will be choosing your very first Pokémon.\n";
-
-    Pokemon chosen_pokemon = Pokemon::InvalidChoice;
-    int choice;
-
-    while (chosen_pokemon == Pokemon::InvalidChoice)
+    Pokemon(string p_name, PokemonType p_type, int p_health) 
     {
-        cout << "You can choose one of the following Pokémon:\n";
-        cout << "1. Bulbasaur\n2. Charmander\n3. Squirtle\n";
-        cout << "Which Pokémon would you like to choose? Enter the number: ";
+        name = p_name;
+        type = p_type;
+        health = p_health;
+    }
 
-        
+    void Attack() 
+    { 
+        cout << name << "Attacks with a powerful move!\n"; 
+    }
+
+};
+
+class Player
+{
+public:
+    string player_name;
+    Pokemon player_pokemon;
+
+    PokemonOptions chosen_pokemon;
+
+    void EnterName()
+    {
+        cin >> player_name;
+
+        cout << "\nWelcome " + player_name + " to the start of your adventure in the Pokemon World." << endl;
+    }
+
+    PokemonOptions ChoosePokemon()
+    {
+        int choice;
+
         cin >> choice;
 
-        switch (choice)
+        chosen_pokemon = (PokemonOptions)choice;
+
+        switch (chosen_pokemon)
         {
-        case 1:
-            chosen_pokemon = Pokemon::Bulbasaur; 
+        case PokemonOptions::Bulbasaur:
+            player_pokemon = Pokemon("Bulbasaur", PokemonType::GRASS, 100);
+            break;
+
+        case PokemonOptions::Charmander:
+            player_pokemon = Pokemon("Charmander", PokemonType::FIRE, 100);
+            break;
+
+        case PokemonOptions::Squirtle:
+            player_pokemon = Pokemon("Squirtle", PokemonType::WATER, 100);
+            break;
+
+        default:
+            chosen_pokemon = PokemonOptions::InvalidChoice;
+            break;
+        }
+
+        return chosen_pokemon;
+    }
+};
+
+class ProfessorOak
+{
+public:
+    ProfessorOak()
+    {}
+
+    void GreetPlayer()
+    {
+        cout << "Hello there! Welcome to the world of Pokemon!\n";
+        cout << "I am Professor Oak. People call me the Pokemon Professor!\n";
+    }
+
+    void AskName()
+    {
+        cout << "\nTrainer! Before you embark on your journey, the Pokemon world needs to know who you are." << endl;
+        cout << "Time to step up and tell us your name and remember a great name is the start of every great adventure." << endl;
+    }
+
+    void PokemonSelectIntro()
+    {
+        cout << "\nToday is a momentous day—you will be choosing your very first Pokémon.\n\n";
+    }
+
+    void OfferPokemonChoices()
+    {
+        cout << "You can choose one of the following Pokémon:\n";
+        cout << "1. Bulbasaur. The grass type. Calm and collected! \n2. Charmander. The fire type. A real hothead!\n3. Squirtle. The water type. Cool as a cucumber!\n";
+        cout << "Which Pokémon would you like to choose? Enter the number: ";       
+    }
+
+    void ChoiceReaction(Player& player)
+    {
+        switch (player.chosen_pokemon)
+        {
+        case PokemonOptions::Bulbasaur:
             cout << "\nYou chose Bulbasaur! A wise choice. Bulbasaur is always ready to grow on you!\n";
             break;
 
-        case 2:
-            chosen_pokemon = Pokemon::Charmander;           
+        case PokemonOptions::Charmander:
             cout << "\nYou chose Charmander! A fiery choice. Charmander is yours!\n";
             break;
 
-        case 3:
-            chosen_pokemon = Pokemon::Squirtle; 
+        case PokemonOptions::Squirtle:
             cout << "\nYou chose Squirtle! A cool choice. Squirtle will keep you cool under pressure!\n";
             break;
 
         default:
-            cout << "Do not have any Pokemon in that slot. These are the only Pokemon you have at the moment.\n";
+            cout << "\nDo not have any Pokemon in that slot. These are the only Pokemon you have at the moment.\n";
             cout << "You will unlock more as you progress.\n";
             cout << "Choose from the available Pokemon\n\n";
             break;
         }
     }
 
-    cout << ChosenPokemonName(chosen_pokemon) + " and you, " + player_name + " are going to be really good friends\n";
-    cout << "You are now ready to embark on your journey! Get ready to explore the vast world of Pokemon!\n";
+    void CongratulatePlayer(Player& player)
+    {  
+        cout << player.player_pokemon.name + " and you, " + player.player_name + " are going to be really good friends\n";
+        cout << "You are now ready to embark on your journey! Get ready to explore the vast world of Pokemon!\n";
+    }
+};
+
+/*string ChosenPokemonName(PokemonOptions choice)
+{
+    switch (choice)
+    {
+    case PokemonOptions::Bulbasaur:
+        return "Bulbasur";
+        break;
+
+    case PokemonOptions::Charmander:
+        return "Charmander";
+        break;
+
+    case PokemonOptions::Squirtle:
+        return "Squirtle";
+        break;
+
+    }
+}*/
+
+int main() {
+    
+    Player player;
+    ProfessorOak oak;
+    //Pokemon pokemon;
+
+    oak.GreetPlayer();
+    oak.AskName();
+
+    player.EnterName();
+    
+    oak.PokemonSelectIntro();
+    oak.OfferPokemonChoices(); 
+
+    while (player.ChoosePokemon() == PokemonOptions::InvalidChoice)
+    {
+        oak.ChoiceReaction(player);
+        oak.OfferPokemonChoices();   
+    }
+
+    oak.ChoiceReaction(player);
+
+    oak.CongratulatePlayer(player);
+
     return 0;
 }
