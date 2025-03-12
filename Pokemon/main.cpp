d6e67ae0455bbd33b82697a2398df7c6546e8405
SidParulekar <76064146+SidParulekar@users.noreply.github.com>
2025-03-12 15:29:37 +0530

    Added a game loop
    Created a new function globally which implements a while loop that displays different actions the player can perform and accepts user input then displays result of the action performed based on user input accordingly using switch statement. The loop ends if the player selects the option to quit.

--- Pokemon/main.cpp
+++ Pokemon/main.cpp
@@ -257,6 +257,54 @@ public:
     }
 }*/
 
+void GameLoop(Player& player)
+{
+    bool keepPlaying = true;
+
+    int choice;
+
+    ClearScreen(); 
+
+    while (keepPlaying) {
+        cout << "What would you like to do next " + player.player_name + "?\n";
+        cout << "1. Battle Wild Pokémon\n";
+        cout << "2. Visit PokeCenter\n";
+        cout << "3. Challenge Gyms\n";
+        cout << "4. Enter Pokémon League\n";
+        cout << "5. Quit\n";
+        cout << "Enter your choice: ";
+        cin >> choice;
+
+        switch (choice)
+        {
+        case 1:
+            cout << "You look around... but all the wild Pokemon are on vacation. Maybe try again later?\n";
+            break;
+
+        case 2:
+            cout << "You head to the PokeCenter, but Nurse Joy is out on a coffee break. Guess your Pokemon will have to tough it out for now!\n";
+            break;
+        
+        case 3:
+            cout << "You march up to the Gym, but it's closed for renovations. Seems like even Gym Leaders need a break!\n";
+            break;
+
+        case 4:
+            cout << "You boldly step towards the Pokemon League... but the gatekeeper laughs and says, 'Maybe next time, champ!\n";
+            break;
+
+        case 5:
+            keepPlaying = false;
+            cout << "Ah seems like you and your pokemon need a break. No worries, rest up for your journey ahead. You will need it.\n";
+            break;
+
+        default:
+            cout << "Invalid choice. Why don't you try again?\n";
+            break;                 
+        }
+    }
+}
+
 int main() {
     
     Player player;
@@ -270,7 +318,10 @@ int main() {
 
     player.EnterName();
 
-    ClearScreen(); 
+    ClearScreen();
+
+    //Clear buffer from the enter button pressed for accepting user input to allow player to press enter for next dialogue
+    cin.ignore(numeric_limits<streamsize>::max(), '\n');
     
     oak.PokemonSelectIntro();
     player.NextDialogue();
@@ -284,6 +335,8 @@ int main() {
         oak.OfferPokemonChoices();   
     }
 
+    cin.ignore(numeric_limits<streamsize>::max(), '\n'); 
+
     oak.ChoiceReaction(player);
     player.NextDialogue();
 
@@ -292,6 +345,8 @@ int main() {
 
     ClearScreen();
     oak.ExplainMainQuest(player);
+
+    GameLoop(player);
 
     return 0;
 }
